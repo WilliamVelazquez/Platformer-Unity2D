3adf78dc6299759a831c18ef179a1d15b03399eb
William Velazquez <william.velazquez.a@gmail.com>
2018-11-02 01:21:04 -0600

    Creating Enemy Sprite and Creating Enemy Behaviour Script with Enemy Movement (Left-Right)

---
+++ Platformer/Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    Rigidbody2D enemyRb;
+    float timeBeforeChange;
+    public float delay = 3.5f;
+    public float speed = 2.5f;
+
+	// Use this for initialization
+	void Start () {
+        enemyRb = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        enemyRb.velocity = Vector2.right * speed;
+
+        if (timeBeforeChange < Time.time)
+        {
+            speed *= -1;
+            timeBeforeChange = Time.time + delay;
+        }
+	}
+}
