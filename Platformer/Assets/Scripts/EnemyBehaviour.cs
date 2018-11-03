642154858e43a32ca146be36e58a911cec064ad7
William Velazquez <william.velazquez.a@gmail.com>
2018-11-03 15:25:07 -0600

    EnemyWalk Animation, EnemyDead Animation and DisableEnemy Code

--- Platformer/Assets/Scripts/EnemyBehaviour.cs
+++ Platformer/Assets/Scripts/EnemyBehaviour.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour {
 
     Rigidbody2D enemyRb;
+    SpriteRenderer enemySpriteRenderer;
+    Animator enemyAnimator;
     float timeBeforeChange;
     public float delay = 3.5f;
     public float speed = 2.5f;
@@ -12,7 +14,8 @@ public class EnemyBehaviour : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         enemyRb = GetComponent<Rigidbody2D>();
-
+        enemySpriteRenderer = GetComponent<SpriteRenderer>();
+        enemyAnimator = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -20,10 +23,32 @@ public class EnemyBehaviour : MonoBehaviour {
 
         enemyRb.velocity = Vector2.right * speed;
 
+        if (speed > 0)
+            enemySpriteRenderer.flipX = false;
+        else if (speed < 0)
+            enemySpriteRenderer.flipX = true;
+
         if (timeBeforeChange < Time.time)
         {
             speed *= -1;
             timeBeforeChange = Time.time + delay;
         }
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            if(transform.position.y + .03f < collision.transform.position.y)
+            {
+                enemyAnimator.SetBool("isDead", true);
+            }
+        }
+    }
+
+    public void DisableEnemy()
+    {
+        gameObject.SetActive(false);
+        //Destroy(gameObject);
+    }
 }
